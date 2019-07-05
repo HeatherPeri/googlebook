b345f3ca85ba4caf9fa2511ae2081ae052ff68e4
Heather Peri <heatherhperi@gmail.com>
2019-07-05 14:42:28 -0400

    added bootstrap and table

--- books/src/App.js
+++ books/src/App.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import './App.css';
+import Search from './components/Search';
+import Saved from './components/Saved';
+
+
 
 function App() {
+  state = {
+    Books: [
+      {
+        _id: 1,
+        title: 'Harry Potter',
+      },
+      
+      {
+        _id: 2,
+        author: 'JK Rowling',
+      },
+      {
+        _id: 3,
+        description: 'Harry and the gang take on spells',
+      },
+      {
+        _id: 4,
+        image: 'Harry Potter',
+      },
+      {
+        _id: 5,
+        link: 'link',
+      }
+    ]
+  }
   return (
     <div className="App">
     <h1>Hello World</h1>
+    <Search />
+    <Saved />
     </div>
   );
 }
--- books/src/components/Search.js
+++ books/src/components/Search.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
+
 function Search() {
   return (
     <div>
-    <h1>Search</h1>
+    <Table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Description</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Book Title</td>
+                <td>Jane Doe</td>
+                <td>Hello from the other side</td>
+                <td>
+                    <button color="danger" size="sm">Delete</button>
+                </td>
+            </tr>
+        </tbody>
+    </Table>
     </div>
   );
 }
